2c45803c26cd14a1bbd931a4b50225426ff2297c
VirtualWolf <virtualwolf@virtualwolf.org>
2017-07-23 17:04:50 +1000

    Added site logo.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Location from './Location.js';
+import title from './title.png';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="App-header">
+                    <img src={title} alt="virtualwolf.cloud" />
                     <h1>virtualwolf.cloud</h1>
                 </div>
                 <div className="App-body">
