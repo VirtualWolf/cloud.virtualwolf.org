58fef698aea5226e37ec64d017fdf62abebf06a0
VirtualWolf <virtualwolf@virtualwolf.org>
2017-07-30 22:35:02 +1000

    Media images now max-width 100%, re-added loading spinner but with relative positioning.

--- src/components/MediaItemList.js
+++ src/components/MediaItemList.js
@@ -6,7 +6,7 @@ class MediaItemList extends Component {
             <div>
                 {this.props.items.map(item => (
                     <div className="Media-item" key={item.id}>
-                        <a href={item.url}><img src={item.thumbnailUrl} alt={item.context} className="Media-image" /></a>
+                        <img src={item.thumbnailUrl} alt={item.context} className="Media-image" />
                         <p>{item.context}</p>
                     </div>
                 ))}
